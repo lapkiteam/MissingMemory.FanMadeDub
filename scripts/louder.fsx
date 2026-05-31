490cff96c10c9651ca5fa5978524a05a926f901f
gretmn102 <erlgreywatson@gmail.com>
2026-05-31 04:31:38 +0300

    feat(scripts/louder): make auto volume

--- scripts/louder.fsx
+++ scripts/louder.fsx
@@ -2,8 +2,43 @@
 #r "nuget: FSharpMyExt, 2.0.0-prerelease.11"
 #load @"TimecodeMediaSplitter/src/ffmpegApi.fsx"
 open FfmpegApi
+open FsharpMyExtension.Containers
+open FsharpMyExtension.Serialization.Serializers
 open FsharpMyExtension.IO
 open System.IO
+
+module Result =
+    let ofOption err = function
+        | None -> Error err
+        | Some x -> Ok x
+
+type ProcResult = int * string * string
+
+module ProcResult =
+    let toResult ((code, stdOut, stdErr): ProcResult) =
+        if code = 0 then
+            Ok stdErr // хрен знает почему. но ffmpeg фигачит всё в err
+        else
+            Error (code, stdOut, stdErr)
+
+[<RequireQualifiedAccess>]
+type MaxVolumeError =
+    | Ffmpeg of ProcResult
+    | MaxVolumeNotFound
+
+let maxVolume input =
+    FfMpeg.startProc
+        (String.concat " " [
+            $"-i \"%s{input}\""
+            "-af \"volumedetect\" -vn -sn -dn -f null -"
+        ])
+    |> ProcResult.toResult
+    |> Result.mapError MaxVolumeError.Ffmpeg
+    |> Result.bind (fun stdOut ->
+        Regex.matchOther """max_volume: (-\d+.\d+) dB""" stdOut
+        |> Result.ofOption MaxVolumeError.MaxVolumeNotFound
+        |> Result.map (Seq.head >> float)
+    )
 
 let louder volume outputDir (inputFile: string) =
     let dest =
@@ -16,15 +51,17 @@ let louder volume outputDir (inputFile: string) =
             $"\"%s{dest}\""
         ])
 
-let louders pattern volume outputDir inputDir =
+let louders pattern outputDir inputDir =
     Directory.EnumerateFiles(inputDir, pattern)
     |> Seq.iter (fun inputFile ->
-        let result =
-            louder volume outputDir inputFile
+        maxVolume inputFile
+        |> Result.iter (fun maxVolume ->
+            let result =
+                louder $"%g{abs maxVolume}dB" outputDir inputFile
+            ()
+        )
         ()
     )
 
-louders "p-*.mp3" "8.055dB" "output" "src/game/voices"
-louders "w-*.mp3" "14.065dB" "output" "src/game/voices"
-// louders "p-*.mp3" 3.5 "output" "src/game/voices"
-// louders "w-*.mp3" 5.0 "output" "src/game/voices"
+louders "p-*.mp3" "output" "src/game/voices"
+louders "w-*.mp3" "output" "src/game/voices"
